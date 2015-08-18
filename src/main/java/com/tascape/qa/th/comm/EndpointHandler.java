9785e7f3f8454d27f9daa60427ef03fc1aa34434
Lindsay Wang <tascape@gmail.com>
2015-08-17 22:52:27 -0700

    add mock for web service

---
+++ src/main/java/com/tascape/qa/th/comm/EndpointHandler.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2015.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tascape.qa.th.comm;
+
+import com.tascape.qa.th.driver.EntityDriver;
+import java.io.IOException;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.nio.protocol.BasicAsyncRequestConsumer;
+import org.apache.http.nio.protocol.BasicAsyncResponseProducer;
+import org.apache.http.nio.protocol.HttpAsyncExchange;
+import org.apache.http.nio.protocol.HttpAsyncRequestConsumer;
+import org.apache.http.nio.protocol.HttpAsyncRequestHandler;
+import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author linsong wang
+ */
+public abstract class EndpointHandler extends EntityDriver implements HttpAsyncRequestHandler<HttpRequest> {
+    private static final Logger LOG = LoggerFactory.getLogger(EndpointHandler.class);
+
+    public abstract String getEndpoint();
+
+    public abstract void handleGet(HttpRequest request, HttpResponse response) throws HttpException, IOException;
+
+    public abstract void handlePost(HttpRequest request, HttpResponse response) throws HttpException, IOException;
+
+    public abstract void handlePut(HttpRequest request, HttpResponse response) throws HttpException, IOException;
+
+    public abstract void handleDelete(HttpRequest request, HttpResponse response) throws HttpException, IOException;
+
+    @Override
+    public HttpAsyncRequestConsumer<HttpRequest> processRequest(HttpRequest hr, HttpContext hc)
+        throws HttpException, IOException {
+        return new BasicAsyncRequestConsumer();
+    }
+
+    @Override
+    public void handle(HttpRequest request, HttpAsyncExchange hae, HttpContext hc) throws HttpException, IOException {
+        String method = request.getRequestLine().getMethod().toUpperCase();
+        HttpResponse response = hae.getResponse();
+        response.setStatusCode(HttpStatus.SC_OK);
+        switch (method) {
+            case "GET":
+                this.handleGet(request, response);
+                break;
+            case "POST":
+                this.handlePost(request, response);
+                break;
+            case "PUT":
+                this.handlePut(request, response);
+                break;
+            case "DELETE":
+                this.handleDelete(request, response);
+                break;
+        }
+
+        hae.submitResponse(new BasicAsyncResponseProducer(response));
+    }
+}
---
+++ src/main/java/com/tascape/qa/th/comm/GenericWebService.java
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2015.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tascape.qa.th.comm;
+
+import com.tascape.qa.th.SystemConfiguration;
+import com.tascape.qa.th.driver.EntityDriver;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.impl.nio.DefaultHttpServerIODispatch;
+import org.apache.http.impl.nio.DefaultNHttpServerConnection;
+import org.apache.http.impl.nio.DefaultNHttpServerConnectionFactory;
+import org.apache.http.impl.nio.reactor.AbstractMultiworkerIOReactor;
+import org.apache.http.impl.nio.reactor.DefaultListeningIOReactor;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.nio.NHttpConnectionFactory;
+import org.apache.http.nio.protocol.HttpAsyncService;
+import org.apache.http.nio.protocol.UriHttpAsyncRequestHandlerMapper;
+import org.apache.http.nio.reactor.IOEventDispatch;
+import org.apache.http.nio.reactor.ListeningIOReactor;
+import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.protocol.HttpProcessorBuilder;
+import org.apache.http.protocol.ResponseConnControl;
+import org.apache.http.protocol.ResponseContent;
+import org.apache.http.protocol.ResponseDate;
+import org.apache.http.protocol.ResponseServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author linsong wang
+ */
+public final class GenericWebService extends EntityDriver {
+    private static final Logger LOG = LoggerFactory.getLogger(GenericWebService.class);
+
+    public static final String SYSPROP_PORT = "qa.th.driver.ws.GWS_PORT";
+
+    private final int port;
+
+    private final UriHttpAsyncRequestHandlerMapper reqistry;
+
+    private final HttpAsyncService protocolHandler;
+
+    ListeningIOReactor ioReactor;
+
+    public GenericWebService(int port) {
+        this.port = port;
+
+        LOG.info("Create HTTP protocol processing chain");
+        HttpProcessor httpProcessor = HttpProcessorBuilder.create()
+            .add(new ResponseDate())
+            .add(new ResponseServer(GenericWebService.class.getName()))
+            .add(new ResponseContent())
+            .add(new ResponseConnControl()).build();
+
+        LOG.info("Create request handler registry");
+        reqistry = new UriHttpAsyncRequestHandlerMapper();
+        protocolHandler = new HttpAsyncService(httpProcessor, reqistry);
+    }
+
+    public void start() throws Exception {
+        LOG.info("Register shutdown handler, GET http://localhost:{}/shutdown to shutdown this service", port);
+        this.registerResponseSimulator(new ShutdownService());
+
+        LOG.info("Create server-side HTTP protocol handler");
+        NHttpConnectionFactory<DefaultNHttpServerConnection> connFactory
+            = new DefaultNHttpServerConnectionFactory(ConnectionConfig.DEFAULT);
+        IOEventDispatch ioEventDispatch = new DefaultHttpServerIODispatch(protocolHandler, connFactory);
+        IOReactorConfig config = IOReactorConfig.custom()
+            .setIoThreadCount(8)
+            .setSoTimeout(300)
+            .setConnectTimeout(300)
+            .build();
+
+        LOG.info("Create server-side I/O reactor");
+        ioReactor = new DefaultListeningIOReactor(config);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    ioReactor.shutdown(100);
+                    LOG.info("Service shutdown");
+                } catch (IOException ex) {
+                    LOG.error("", ex);
+                }
+            }
+        });
+
+        try {
+            SocketAddress socketAddress = new InetSocketAddress(this.port);
+            ioReactor.listen(socketAddress);
+            LOG.info("Service is ready");
+            ioReactor.execute(ioEventDispatch);
+        } catch (InterruptedIOException ex) {
+            LOG.error("Interrupted", ex);
+        } catch (IOException ex) {
+            LOG.error("I/O error: ", ex);
+        } finally {
+            if (ioReactor instanceof AbstractMultiworkerIOReactor) {
+                AbstractMultiworkerIOReactor amior = (AbstractMultiworkerIOReactor) ioReactor;
+                amior.getAuditLog().stream().forEach(ex -> LOG.warn(ex.getTimestamp() + " - {}", ex.getCause()));
+            }
+        }
+    }
+
+    public void registerResponseSimulator(EndpointHandler simulator)
+        throws InstantiationException, IllegalAccessException {
+        LOG.info("Register handler for endpoint {}: {}", simulator.getEndpoint(), simulator.getName());
+        reqistry.register(simulator.getEndpoint(), simulator);
+    }
+
+    public void registerResponseSimulator(String clazz)
+        throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        EndpointHandler simulator = (EndpointHandler) Class.forName(clazz).newInstance();
+        reqistry.register(simulator.getEndpoint(), simulator);
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public void reset() throws Exception {
+        LOG.info("nothing to reset");
+    }
+
+    private class ShutdownService extends EndpointHandler {
+        @Override
+        public String getEndpoint() {
+            return "/shutdown";
+        }
+
+        @Override
+        public void handleGet(HttpRequest request, HttpResponse response) throws HttpException, IOException {
+            LOG.debug("received shutdown call");
+            ioReactor.shutdown();
+        }
+
+        @Override
+        public String getName() {
+            return ShutdownService.class.getName();
+        }
+
+        @Override
+        public void reset() throws Exception {
+            LOG.debug("NA");
+        }
+
+        @Override
+        public void handlePost(HttpRequest request, HttpResponse response) throws HttpException, IOException {
+            response.setStatusCode(HttpStatus.SC_NOT_ACCEPTABLE);
+        }
+
+        @Override
+        public void handlePut(HttpRequest request, HttpResponse response) throws HttpException, IOException {
+            response.setStatusCode(HttpStatus.SC_NOT_ACCEPTABLE);
+        }
+
+        @Override
+        public void handleDelete(HttpRequest request, HttpResponse response) throws HttpException, IOException {
+            response.setStatusCode(HttpStatus.SC_NOT_ACCEPTABLE);
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = SystemConfiguration.getInstance().getIntProperty(SYSPROP_PORT, 10080);
+        GenericWebService gws = new GenericWebService(port);
+        try {
+            gws.start();
+        } catch (Exception ex) {
+            LOG.error("Cannot start service", ex);
+        }
+    }
+}
